33a12d739dd3587ad9eeea494fe3e38ce4771afc
jackehogan <hoganjacke@gmail.com>
2022-07-16 06:00:36 -0700

    first commit

---
+++ website/views.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note, User
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
+
+@views.route('/qrewards', methods=['GET'])
+def qrewards():
+    users = User.query.all()
+    deals = [note for user in users for note in user.notes]
+    print(deals)
+    for user in users:
+        for note in user.notes:
+            print(note.data)
+
+    return render_template("qrewards.html",user=current_user,note = note)
